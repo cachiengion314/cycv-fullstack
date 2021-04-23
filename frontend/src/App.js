abe33ace5d8e7ac30fac75c189a238cf231b0c0e
Thanh Tung <rubyblacklove@gmail.com>
2021-04-23 21:49:29 +0700

    test push lan 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from "./components/sign-compoents/sign-in"
+import Test from "./components/about-us"
 
 function App() {
   return (
     <div className="App">
       <SignIn/>
-      
+      <Test/>
     </div>
   );
 }
