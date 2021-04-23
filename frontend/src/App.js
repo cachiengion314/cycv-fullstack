43e190efa85d31df84082dcc42be883694469ae5
Hoang_Nam <cachiengion314@gmail.com>
2021-04-23 22:52:32 +0700

    fix conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from "./components/sign-compoents/sign-in"
-<<<<<<< HEAD
 import SignUp from "./components/sign-compoents/sign-up"
 function App() {
   return (
     <div className="App">
       <SignUp/>
      
-=======
-import Test from "./components/about-us"
-
-function App() {
-  return (
-    <div className="App">
-      <SignIn/>
-      <Test/>
->>>>>>> abe33ace5d8e7ac30fac75c189a238cf231b0c0e
     </div>
   );  
 }
