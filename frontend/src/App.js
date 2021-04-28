be79b8a15562f6c96280efc3361529d6eca3343c
Thanh Tung <rubyblacklove@gmail.com>
2021-04-28 15:40:34 +0700

    Cap nhat About us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from "./components/sign-compoents/sign-in"
 import SignUp from "./components/sign-compoents/sign-up"
+import AboutUs from "./components/about-us/index"
 function App() {
   return (
     <div className="App">
-      <SignUp/>
-     
+      {/* <SignUp/> */}
+     <AboutUs/>
     </div>
   );  
 }
--- frontend/src/components/about-us/index.js
+++ frontend/src/components/about-us/index.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import "./index.css"
+import image from './image/cv-image.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 
 const index = () => {
     return (
-        <div >
-              <div className="bg-img">
-  <form action="/action_page.php" class="container">
-    <h1>Login</h1>
+        <div className="Navbar">
+            <Navbar collapseOnSelect expand="lg" bg="ligh" variant="ligh" className="margin">
+                <Navbar.Brand href="#home">Mindx Create your CV</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavDropdown title="My CV" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#About US">About US</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm" />
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                    <Nav className="in-up">
+                        <Nav.Link href="#deets">Sign in</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">Sign up</Nav.Link>
+                        <Nav.Link href="#Contact US">Contact US</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className="container">
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required/>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
-
-    <button type="submit" class="btn">Login</button>
-  </form>
-</div>
+                <div>
+                    <p>Hãy cho tôi cả thế giới tôi sẽ làm nên tất cả</p>
+                </div>
+                <div>
+                    <img src={image} className="image"></img>
+                </div>
+                <div>
+                    <p>Tôi có thể cân cả thế giới</p>
+                </div>
+            </div>
         </div>
+
     )
 }
 
