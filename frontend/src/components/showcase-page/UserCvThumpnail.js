df357b625b18daa632b3a99a753f5a6634306714
Hoang_Nam <cachiengion314@gmail.com>
2021-05-10 18:50:41 +0700

    fix text-not-wrap in thumbnail ver: 1.0.6 - stable

--- frontend/src/components/showcase-page/UserCvThumpnail.js
+++ frontend/src/components/showcase-page/UserCvThumpnail.js
@@ -6,6 +6,16 @@ import Vars from '../other-stuffs/Vars'
 import styled from "styled-components"
 import useRoute from '../authenticate/useRoute'
 
+const TextWrap = styled(Text)`
+-ms-word-break: break-all;
+word-break: break-all;
+word-break: break-word;
+
+-webkit-hyphens: auto;
+-moz-hyphens: auto;
+hyphens: auto;
+`
+
 const BlockHover = styled(Block)`
     cursor: pointer;
     &:hover {
@@ -24,8 +34,8 @@ const UserCvThumpnail = ({ width, createdBy, saveDataId, thumbnailUrl, fileName,
     return (
         <BlockHover width={width} onClick={handleThumnailClick} padding="1rem" borderRadius=".5rem" border="1px solid darkblue" className={className}>
             <Avatar width="100%" src={thumbnailUrl || "https://res.cloudinary.com/cachiengion314/image/upload/v1619364371/cv_template_2_rl98uw.jpg"} alt="Cv-thumnail" className="mb-2" />
-            <Text width="100%" fontSize={Vars.FONT_SIZE_SSM} textAlign="center" className="text-secondary">{createdBy}</Text>
-            <Text width="100%" fontSize={Vars.FONT_SIZE_SSM} textAlign="center" className=" text-primary">{fileName}</Text>
+            <TextWrap width="100%" fontSize={Vars.FONT_SIZE_SSM} textAlign="center" className="text-secondary">{createdBy}</TextWrap>
+            <TextWrap width="100%" fontSize={Vars.FONT_SIZE_SSM} textAlign="center" className=" text-primary">{fileName}</TextWrap>
         </BlockHover>
     )
 }
