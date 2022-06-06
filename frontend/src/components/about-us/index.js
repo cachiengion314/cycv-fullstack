29af570150bc73cbe19abb24a6520b4017c555ae
Hoang_Nam <cachiengion314@gmail.com>
2022-06-06 18:59:15 +0700

    change footer mb-2

--- frontend/src/components/about-us/index.js
+++ frontend/src/components/about-us/index.js
@@ -7,7 +7,7 @@ const index = ({ width, className }) => {
   const image = "https://res.cloudinary.com/cachiengion314/image/upload/v1619718969/cv-image_jhl0qo.png"
   return (
     <div style={{ width: width, padding: "0 2rem" }} className={className}>
-      <Text width="100%" textAlign="center" className="mt-5">Trang web này thuộc về cachiengion314</Text>
+      <Text width="100%" textAlign="center" className="mt-5 mb-2">Trang web này thuộc về cachiengion314</Text>
       <div className={`about-us-container ${className}`}>
         <div style={{ width: "100%" }} className="align-center">
           <img src={image} className="cycv-image me-3" alt="about-us"></img>
